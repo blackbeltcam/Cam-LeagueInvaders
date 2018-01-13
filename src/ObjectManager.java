f62e54e43c4c84aeed3c9bed324b010e058ba548
blackbeltcam <league@WTS20.attlocal.net>
2018-01-12 17:09:25 -0800

    league invaders

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,10 +1,13 @@
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ObjectManager {
 	RocketShip rs;
 	ArrayList<Projectile>projectile=new ArrayList<Projectile>();
 	ArrayList<Alien>alien=new ArrayList<Alien>();
+	long enemyTimer = 0;
+	int enemySpawnTime = 1000;		
 	public ObjectManager(RocketShip rs) {
 		this.rs=rs;
 		
@@ -29,6 +32,18 @@ public class ObjectManager {
 	}
 	public void addProjectile(Projectile pr) {
 		projectile.add(pr);
+	}
+	public void manageEnemies(){
+        if(System.currentTimeMillis() - enemyTimer >= enemySpawnTime){
+                addAlien(new Alien(new Random().nextInt(LeagueInvaders.WIDTH), 0, 50, 50));
+
+enemyTimer = System.currentTimeMillis();
+        }
+}
+	private void purgeObjects() {
+		for(int i=0; projectile.get(i);i++) {
+			
+		}
 	}
 	public void addAlien(Alien al) {
 		alien.add(al);
